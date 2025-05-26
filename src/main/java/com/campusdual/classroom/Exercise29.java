7b126e622c3f72f44f04c4aabb3ba7d6a2cdd49f
Pablo <pablobarreiro131@gmail.com>
2025-05-26 11:37:06 +0200

    Ejercicio 29. Completado y testeado.

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -2,6 +2,14 @@ package com.campusdual.classroom;
 
 public class Exercise29 {
     public static void main(String[] args) {
-
+        int [] array = new int [2];
+        try{
+            for (int i = 0; i<5 ; i++){
+                array[i]=i;
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("No hay espacio en el array.");
+        }
     }
 }
